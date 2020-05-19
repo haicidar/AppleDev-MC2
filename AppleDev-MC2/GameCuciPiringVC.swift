882cbebf7432e77e24392ff24aafb125128e56a6
intan318 <34388370+intan318@users.noreply.github.com>
2020-05-19 16:01:01 +0700

    added UIPanGestureRecognizer to the sponge

--- AppleDev-MC2/GameCuciPiringVC.swift
+++ AppleDev-MC2/GameCuciPiringVC.swift
@@ -9,22 +9,28 @@
 import UIKit
 
 class GameCuciPiringVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
     }
     
+   @IBAction func handlePan(_ gesture: UIPanGestureRecognizer) {
+     // 1
+     let translation = gesture.translation(in: view)
 
-    /*
-    // MARK: - Navigation
+     // 2
+     guard let gestureView = gesture.view else {
+       return
+     }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     gestureView.center = CGPoint(
+       x: gestureView.center.x + translation.x,
+       y: gestureView.center.y + translation.y
+     )
+
+     // 3
+     gesture.setTranslation(.zero, in: view)
+   }
 
 }
