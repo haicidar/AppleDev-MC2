baf3d45152991e6d79af47579e8afeeb490e599d
I Dewa Agung Ary Aditya Wibawa <idewaagungaryadityawibawa@MacBook-Pro-I.local>
2020-05-25 13:39:19 +0700

    Game Cuci Tangan

---
+++ AppleDev-MC2/HintCuciTanganVC.swift
@@ -0,0 +1,28 @@
+//
+//  HintCuciTanganVC.swift
+//  AppleDev-MC2
+//
+//  Created by I Dewa Agung Ary Aditya Wibawa on 19/05/20.
+//  Copyright © 2020 Group-1. All rights reserved.
+//
+
+import UIKit
+
+class HintCuciTanganVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.6)
+        view.isOpaque = false
+        
+        //Adding Tap Recognizer to dismiss the hint
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissHint")
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Function for dismiss the Hint
+    @objc func dismissHint(){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
